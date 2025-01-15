55b7fb097aafe67d287d84bb9f1d8de3bd1e7aca
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-01-15 19:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (21.70%) - LeetSync

--- 2509-minimize-xor/minimize-xor.java
+++ 2509-minimize-xor/minimize-xor.java
@@ -1,36 +1,18 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        int targetBits = 0;
-        while(num2>0){
-            targetBits += num2&1;
-            num2>>=1;
-        }
-        int setBits = 0;
-        int temp = num1;
-        while(temp>0){
-            setBits += temp&1;
-            temp>>=1;
-        }
-        if(setBits==targetBits) return num1;
-        if(setBits<targetBits){
-            int result = num1, remaining = targetBits-setBits;
-            for(int i=0;i<32 && remaining > 0;i++){
-                if((result & (1<<i)) == 0){
-                    result |= (1<<i);
-                    remaining--;
-                }
+        int difference = Integer.bitCount(num2) - Integer.bitCount(num1);
+        int curr = 1, result = num1;
+        while(difference!=0){
+            if(difference<0 && (result&curr)!=0){
+                result ^= curr;
+                difference++;
             }
-            return result;
-        }
-        else{
-            int result = 0;
-            for(int i=31;i>=0&&targetBits>0;i--){
-                if((num1 & (1<<i)) != 0){
-                    result |= (1<<i);
-                    targetBits--;
-                }
+            if(difference>0 && (result&curr)==0){
+                result |= curr;
+                difference--;
             }
-            return result;
+            curr <<= 1;
         }
+        return result;
     }
 }
